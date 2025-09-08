55b08e008968f8d5b36bd89351aaa057cdba29d0
Micah <MJBaca@student.fullsail.edu>
2025-09-07 20:25:15 -0500

    Jest Testing Assignment

---
+++ add.test.js
@@ -0,0 +1,31 @@
+const { add, subtract, multiply, divide, sqrt, max } = require("./index");
+
+// Add
+test("Adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+// Subtract
+test("Subtracts 5 - 2 to equal 3", () => {
+  expect(subtract(5, 2)).toBe(3);
+});
+
+// Multiply
+test("Multiplies 3 * 4 to equal 12", () => {
+  expect(multiply(3, 4)).toBe(12);
+});
+
+// Divide
+test("Divides 10 / 2 to equal 5", () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+// Square root
+test("Square root of 16 to equal 4", () => {
+  expect(sqrt(16)).toBe(4);
+});
+
+// Max
+test("Max of 7 and 10 to equal 10", () => {
+  expect(max(7, 10)).toBe(10);
+});
---
+++ index.js
@@ -0,0 +1,32 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function sqrt(num) {
+  return Math.sqrt(num);
+}
+
+function max(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+  sqrt,
+  max,
+};
